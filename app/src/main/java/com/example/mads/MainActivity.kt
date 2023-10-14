0a5956edc5d748e9350468796a1615ca34f735dd
fwvu <bryanfvu@gmail.com>
2023-10-14 19:32:48 +1100

    1st commit login screen

---
+++ app/src/main/java/com/example/mads/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mads
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.example.mads.MainDashboard.MainDashboard
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var button: AppCompatButton? = null
+
+    private val startApp by lazy {
+        Intent(this, MainDashboard::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.startButton) as? AppCompatButton
+
+        button?.setOnClickListener {
+            startActivity(startApp)
+        }
+    }
+}
